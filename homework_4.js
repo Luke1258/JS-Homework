d7c5cd086a26fca67728458b0a6c3c5d55a773e9
Lukas Zradula <luke1258@seznam.cz>
2023-12-11 15:42:57 +0100

    update homework_4.js about function

--- homework_4.js
+++ homework_4.js
@@ -1,20 +1,29 @@
+// const updateSkill = (id, value) => {
+//     document.querySelector(`#${id}`).style = `width: ${value}%`
+// }
+
+// const html = document.querySelector("#skill01")
+// html.textContent = `${htmlLevel} / 100`
+// updateSkill("skill__progress01", htmlLevel)
+
+// const css = document.querySelector("#skill02")
+// css.textContent = `${cssLevel} / 100`
+// updateSkill("skill__progress02", cssLevel)
+
+// const js = document.querySelector("#skill03")
+// js.textContent = `${jsLevel} / 100`
+// updateSkill("skill__progress03", jsLevel)
 
 const htmlLevel = prompt("Fill your level of HTML")
 const cssLevel = prompt("Fill your level of CSS")
 const jsLevel = prompt("Fill your level of JavaScript")
 
-const updateSkill = (id, value) => {
-    document.querySelector(`#${id}`).style = `width: ${value}%`
+const updateSkill = (skillId, skillProgessId, level) => {
+    const html = document.querySelector(`#${skillId}`)
+    html.textContent = `${level} / 100`
+    document.querySelector(`#${skillProgessId}`).style = `width: ${level}%`
 }
 
-const html = document.querySelector("#skill01")
-html.textContent = `${htmlLevel} / 100`
-updateSkill("skill__progress01", htmlLevel)
-
-const css = document.querySelector("#skill02")
-css.textContent = `${cssLevel} / 100`
-updateSkill("skill__progress02", cssLevel)
-
-const js = document.querySelector("#skill03")
-js.textContent = `${jsLevel} / 100`
-updateSkill("skill__progress03", jsLevel)
+updateSkill("skill01", "skill__progress01", htmlLevel)
+updateSkill("skill02", "skill__progress02", cssLevel)
+updateSkill("skill03", "skill__progress03", jsLevel)
